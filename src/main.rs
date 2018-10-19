5cabe3847ea0dad162f15cddf6d4c666cdb8bbc7
longfangsong <longfangsong@icloud.com>
2018-10-19 12:33:24 +0800

    let's display something!

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,15 @@ extern crate bootloader_precompiled;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod drive;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
+    let mut dummy = 0;
+    for j in 0..10000 {
+        dummy ^= j;
     }
+    drive::vga::VGA.print_int(-123456);
     loop {}
 }
 
