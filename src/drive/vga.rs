fa9890b6ec05dfd7379c9f3b0e33bfd530d260f1
longfangsong <longfangsong@icloud.com>
2018-10-22 23:06:59 +0800

    Don't know how to read memory TAT

--- src/drive/vga.rs
+++ src/drive/vga.rs
@@ -1,44 +1,63 @@
+const WIDTH: isize = 80;
+
 pub struct Vga {
     vga_memory: *mut u8,
-    cursor: isize,
+    cursor_x: isize,
+    cursor_y: isize,
 }
 
 impl Vga {
-    pub fn print_char(mut self, ch: u8) -> Vga {
-        unsafe {
-            *self.vga_memory.offset(self.cursor * 2) = ch;
-            *self.vga_memory.offset(self.cursor * 2 + 1) = 0xb;
+    pub fn new() -> Vga {
+        Vga {
+            vga_memory: 0xb8000 as *mut u8,
+            cursor_x: 0,
+            cursor_y: 0,
         }
-        self.cursor += 1;
+    }
+    pub fn print_char(&mut self, ch: u8) -> &mut Vga {
+        if ch == b'\n' {
+            self.println(b"");
+        } else {
+            unsafe {
+                *self.vga_memory.offset((self.cursor_y * WIDTH + self.cursor_x) * 2) = ch;
+                *self.vga_memory.offset((self.cursor_y * WIDTH + self.cursor_x) * 2 + 1) = 0xb;
+            }
+            self.cursor_x += 1;
+            if self.cursor_x == 80 {
+                self.cursor_x = 0;
+                self.cursor_y += 1;
+            }
+        }
         self
     }
-    pub fn print(mut self, str: &[u8]) -> Vga {
+    pub fn print(&mut self, str: &[u8]) -> &mut Vga {
         for (_, &byte) in str.iter().enumerate() {
-            self = self.print_char(byte)
+            self.print_char(byte);
         }
         self
     }
-    pub fn print_uint(mut self, number: u64) -> Vga {
+    pub fn println(&mut self, str: &[u8]) -> &mut Vga {
+        self.print(str);
+        self.cursor_x = 0;
+        self.cursor_y += 1;
+        self
+    }
+    pub fn print_uint(&mut self, number: u64) -> &mut Vga {
         if number < 10 {
-            self = self.print_char(number as u8 + b'0');
+            self.print_char(number as u8 + b'0');
         } else {
-            self = self.print_uint(number / 10);
-            self = self.print_char((number % 10) as u8 + b'0');
+            self.print_uint(number / 10);
+            self.print_char((number % 10) as u8 + b'0');
         }
         self
     }
-    pub fn print_int(mut self, number: i64) -> Vga {
+    pub fn print_int(&mut self, number: i64) -> &mut Vga {
         if number < 0 {
-            self = self.print_char(b'-');
-            self = self.print_int(-number);
+            self.print_char(b'-');
+            self.print_int(-number);
         } else {
-            self = self.print_uint(number as u64);
+            self.print_uint(number as u64);
         }
         self
     }
 }
-
-pub const VGA: Vga = Vga {
-    vga_memory: 0xb8000 as *mut u8,
-    cursor: 0,
-};
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ extern crate bootloader_precompiled;
 
 use bootloader_precompiled::bootinfo;
 use core::panic::PanicInfo;
+use memory::free_page;
+use memory::pages_available;
 
 mod drive;
+mod memory;
 
 fn main(bootinfo: &'static bootinfo::BootInfo) -> ! {
     const MEMORY_REGION_TYPE: [&[u8; 11]; 14] = [
@@ -26,16 +29,26 @@ fn main(bootinfo: &'static bootinfo::BootInfo) -> ! {
         b"BootInfo   ",
         b"Package    "
     ];
-    for i in 0...14 {
-        drive::vga::VGA
-            .print_uint(i)
+    let mut vga: drive::vga::Vga = drive::vga::Vga::new();
+    vga.print_uint(bootinfo.memory_map.len() as u64)
+        .println(b"");
+    for i in 0..=14 {
+        vga.print_uint(i)
             .print_char(b':')
             .print(MEMORY_REGION_TYPE[bootinfo.memory_map[i as usize].region_type as usize])
             .print_char(b',')
             .print_uint(bootinfo.memory_map[i as usize].range.start_frame_number as u64)
-            .print_char(b't')
-            .print_uint(bootinfo.memory_map[i as usize].range.end_frame_number as u64);
+            .print(b" to ")
+            .print_uint(bootinfo.memory_map[i as usize].range.end_frame_number as u64)
+            .println(b"");
     }
+    for i in 0..10000000 {}
+    unsafe {
+        vga.print_uint(*((1048584 * 4096) as *mut u64));
+        for i in 0..10000000 {}
+    }
+//    free_page((1069) as *mut u8);
+//    vga.print_uint((1070 * 4096) as *mut u8 as u64);
     loop {}
 }
 
